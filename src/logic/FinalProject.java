7f0cbf8d78f132724ea0e805e8420f3b8691a128
Samuel Cam <65782952+makore3@users.noreply.github.com>
2025-08-24 22:44:24 +0200

    Initial commit: Raffle Draw App (Java Swing)

---
+++ src/logic/FinalProject.java
@@ -0,0 +1,13 @@
+package logic;
+
+import gui.Principal;
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
